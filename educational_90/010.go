3d485291842b68fe9b99a06e1b2a31a0d7425fb1
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-05-25 17:20:58 +0900

    Add

---
+++ educational_90/010.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var a [100001]struct{ c, p int }
+var b [100001]struct{ l, r int }
+var s1 [100001]int
+var s2 [100001]int
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	for i := 1; i <= n; i++ {
+		fmt.Fscan(reader, &a[i].c)
+		fmt.Fscan(reader, &a[i].p)
+	}
+
+	var sum1, sum2 int
+	for i := 0; i <= n; i++ {
+		if a[i].c == 1 {
+			sum1 += a[i].p
+		} else {
+			sum2 += a[i].p
+		}
+		s1[i] = sum1
+		s2[i] = sum2
+	}
+
+	var q int
+	fmt.Fscan(reader, &q)
+	for i := 1; i <= q; i++ {
+		fmt.Fscan(reader, &b[i].l)
+		fmt.Fscan(reader, &b[i].r)
+	}
+
+	for i := 1; i <= q; i++ {
+		fmt.Fprintf(writer, "%d %d\n", s1[b[i].r]-s1[b[i].l-1], s2[b[i].r]-s2[b[i].l-1])
+	}
+}
+
+// snip
+// for i := 0; i < n; i++ {
+// for j := 0; j < n; j++ {
+// }
+// }
