9ef9f01451cadf8853edfae328908918b24bc134
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-06-07 22:58:21 +0900

    Add nextPermutation

---
+++ algo/next_permutation.go
@@ -0,0 +1,37 @@
+package main
+
+func swap(a []int, i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func reverse(a []int, i, j int) {
+	for k := 0; k < (j-i+1)/2; k++ {
+		swap(a, i+k, j-k)
+	}
+}
+
+// Narayana Pandita’s algorithm
+func nextPermutation(a []int) {
+	var lastIndex = len(a) - 1
+
+	if lastIndex < 1 {
+		return
+	}
+
+	var i = lastIndex - 1
+	for i >= 0 && a[i] >= a[i+1] {
+		i -= 1
+	}
+	if i < 0 {
+		reverse(a, 0, lastIndex)
+		return
+	}
+
+	var j = lastIndex
+	for j > i+1 && a[j] <= a[i] {
+		j -= 1
+	}
+
+	swap(a, i, j)
+	reverse(a, i+1, lastIndex)
+}
