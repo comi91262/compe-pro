e6f52556bdbd1dbdadb8823b76d07e8545e66fed
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-06-09 01:50:46 +0900

    Add educational_90/003.go

--- educational_90/003.go
+++ educational_90/003.go
@@ -1,38 +1,6 @@
 package main
 
 import (
-	"fmt"
-)
-
-var g [][]int
-var memo []bool
-
-func dfs(n int) {
-	if memo[n] {
-		return
-	}
-	memo = true
-
-	for _, next := range g[n] {
-		dfs(next)
-	}
-}
-
-func main() {
-	var n, m int
-	fmt.Fscan(reader, &n, &m)
-
-	g = make([][]int, n+1)
-	var a, b int
-	for i := 0; i < m; i++ {
-		fmt.Fscan(reader, &a, &b)
-		g[a] = append(g[a], b)
-	}
-
-}
-package main
-
-import (
 	"bufio"
 	"fmt"
 	"os"
@@ -81,6 +49,8 @@ func main() {
 	memo = make([]int, n+1)
 	var _, mn = dfs(1, 0)
 
+	memo = make([]int, n+1)
+	var md, _ = dfs(mn, 0)
+
 	fmt.Fprintf(writer, "%v\n", md+1)
 }
-
