4366958c8114180e9e7e504d51ffa679696e480a
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-06-17 10:10:10 +0900

    Add educational_90/{026.go,028.go}

---
+++ algo/imos2.go
@@ -0,0 +1,21 @@
+for i := 0; i < n; i++ {
+	var lx, ly, rx, ry int
+	fmt.Fscan(reader, &lx, &ly, &rx, &ry)
+
+	a[lx][ly]++
+	a[lx][ry]--
+	a[rx][ly]--
+	a[rx][ry]++
+}
+
+for i := 0; i < w; i++ {
+	for j := 0; j < w; j++ {
+		a[i][j+1] += a[i][j]
+	}
+}
+
+for i := 0; i < w; i++ {
+	for j := 0; j < w; j++ {
+		a[i+1][j] += a[i][j]
+	}
+}
