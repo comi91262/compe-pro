ccd82c6ee5086b4acf446067e1f4cfe2b398284e
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-06-25 22:21:05 +0900

    Add

--- algo/dp.go
+++ algo/dp.go
@@ -1,5 +1,21 @@
 package algo
 
+func treeDp(n, pre int) {
+	var dp [100001]int
+	var g [][]int
+	dp[n]++
+
+	for _, next := range g[n] {
+		if pre == next {
+			continue
+		}
+
+		treeDp(next, n)
+		dp[n] += dp[next]
+	}
+
+}
+
 func substring() {
 	var dp [101][100001]bool
 	var n, sum int
