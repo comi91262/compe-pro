3c11ad49d3a1d2a4891690770259336b186a7557
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-06-29 00:04:31 +0900

    Update main.go

--- main.go
+++ main.go
@@ -11,13 +11,16 @@ var writer = bufio.NewWriter(os.Stdout)
 
 func main() {
 	defer writer.Flush()
-
-	var n, k int
-	fmt.Fscan(reader, &n, &k)
-
-	fmt.Fprintf(writer, "%d\n", n)
+	fmt.Fprintf(writer, "%v\n", ans)
 }
 
+// var n, q int
+// fmt.Fscan(reader, &n, &q)
+
+// var a = make([]int, n)
+// for i := 0; i < n; i++ {
+// 	fmt.Fscan(reader, &a[i])
+// }
 // var n int
 // fmt.Fscan(reader, &n)
 // fmt.Fprintf(writer, "%d\n", n)
@@ -33,12 +36,46 @@ func main() {
 // for j := 0; j < n; j++ {
 // }
 // }
+
+// var g = make([][]int, n+1)
+// var a = make([]int, m+1)
+// var b = make([]int, m+1)
+// for i := 1; i < m+1; i++ {
+// 	fmt.Fscan(reader, &a[i], &b[i])
+// 	g[a[i]] = append(g[a[i]], b[i])
+// 	g[b[i]] = append(g[b[i]], a[i])
+// }
+
+// m := map[int]int{}
+// a := make([]int, n)
+// for i := 0; i < len(a); i++ {
+// 	m[a[i]]++
+// }
+
 // func abs(x int) int {
-// 	return int(math.Abs(float64(x)))
+// 	if x >= 0 {
+// 		return x
+// 	} else {
+// 		return x * -1
+// 	}
 // }
-// func min(x, y int) int {
-// 	return int(math.Min(float64(x), float64(y)))
+//
+// func min(arg ...int) int {
+// 	min := arg[0]
+// 	for _, x := range arg {
+// 		if min > x {
+// 			min = x
+// 		}
+// 	}
+// 	return min
 // }
-// func max(x, y int) int {
-// 	return int(math.Max(float64(x), float64(y)))
+//
+// func max(arg ...int) int {
+// 	max := arg[0]
+// 	for _, x := range arg {
+// 		if max < x {
+// 			max = x
+// 		}
+// 	}
+// 	return max
 // }
