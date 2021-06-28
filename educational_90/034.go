0c878538c448a61e9169fa0e9697505870b1d999
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-06-29 00:03:38 +0900

    Update educational_90/034.go

--- educational_90/034.go
+++ educational_90/034.go
@@ -6,34 +6,6 @@ import (
 	"os"
 )
 
-func abs(x int) int {
-	if x >= 0 {
-		return x
-	} else {
-		return x * -1
-	}
-}
-
-func min(arg ...int) int {
-	min := arg[0]
-	for _, x := range arg {
-		if min > x {
-			min = x
-		}
-	}
-	return min
-}
-
-func max(arg ...int) int {
-	max := arg[0]
-	for _, x := range arg {
-		if max < x {
-			max = x
-		}
-	}
-	return max
-}
-
 var reader = bufio.NewReader(os.Stdin)
 var writer = bufio.NewWriter(os.Stdout)
 
@@ -72,41 +44,3 @@ func main() {
 
 	fmt.Fprintf(writer, "%v\n", ans)
 }
-
-// var n, q int
-// fmt.Fscan(reader, &n, &q)
-
-// var a = make([]int, n)
-// for i := 0; i < n; i++ {
-// 	fmt.Fscan(reader, &a[i])
-// }
-// var n int
-// fmt.Fscan(reader, &n)
-// fmt.Fprintf(writer, "%d\n", n)
-
-//	var n int
-//	fmt.Fscan(reader, &n)
-//	var a = make([]int, n)
-//	for i := 0; i < n; i++ {
-//		fmt.Fscan(reader, &a[i])
-//	}
-// const d = 1_000_000_000 + 7
-// for i := 0; i < n; i++ {
-// for j := 0; j < n; j++ {
-// }
-// }
-
-// var g = make([][]int, n+1)
-// var a = make([]int, n+1)
-// var b = make([]int, n+1)
-// for i := 1; i < n; i++ {
-// 	fmt.Fscan(reader, &a[i], &b[i])
-// 	g[a[i]] = append(g[a[i]], b[i])
-// 	g[b[i]] = append(g[b[i]], a[i])
-// }
-
-// m := map[int]int{}
-// a := make([]int, n)
-// for i := 0; i < len(a); i++ {
-// 	m[a[i]]++
-// }
