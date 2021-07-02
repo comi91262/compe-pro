81ebe83d181a902f543f11ac000805e19ca3bdc3
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-07-02 12:05:45 +0900

    Update binarySearch

---
+++ algo/search.go
@@ -0,0 +1,42 @@
+package algo
+
+func upperBound(value, boader int) bool {
+	return boader < value
+}
+
+func lowerBound(value, boader int) bool {
+	return boader <= value
+}
+
+func binarySearch(a []int, boader int, criteria func(value, boader int) bool) int {
+	abs := func(x int) int {
+		if x >= 0 {
+			return x
+		} else {
+			return x * -1
+		}
+	}
+
+	ng := -1
+	ok := len(a)
+
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if criteria(mid, boader) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+	return ok
+}
+
+func bitFullSearch(n int) {
+	for i := 0; i < 1<<n; i++ {
+		for j := 0; j < n; j++ {
+			if i&(1<<j) != 0 {
+			}
+		}
+	}
+}
