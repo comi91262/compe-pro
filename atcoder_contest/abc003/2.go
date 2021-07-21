5a0163fe89bdb157d8d01632be81a3cfeef98b59
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-22 07:24:06 +0900

    [AC] abc003 2

---
+++ atcoder_contest/abc003/2.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func contains(x byte, a []byte) bool {
+	for _, e := range a {
+		if x == e {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	defer writer.Flush()
+
+	var s, t string
+	fmt.Fscan(reader, &s, &t)
+
+	for i := range s {
+		if t[i] == '@' && s[i] == '@' {
+			continue
+		}
+
+		if s[i] == '@' {
+			if contains(t[i], []byte{byte('a'), byte('t'), byte('c'), byte('o'), byte('d'), byte('e'), byte('r')}) {
+				continue
+			}
+		}
+
+		if t[i] == '@' {
+			if contains(s[i], []byte{byte('a'), byte('t'), byte('c'), byte('o'), byte('d'), byte('e'), byte('r')}) {
+				continue
+			}
+		}
+
+		if s[i] != t[i] {
+			fmt.Fprintf(writer, "%v\n", "You will lose")
+			return
+		}
+	}
+
+	fmt.Fprintf(writer, "%v\n", "You can win")
+}
+
+
