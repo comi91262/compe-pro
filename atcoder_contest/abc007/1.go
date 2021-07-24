0dfcddbecebf4ddcd3d050621427065e9fbbef86
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-25 07:23:40 +0900

    [AC] abc007 1

---
+++ atcoder_contest/abc007/1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	fmt.Fprintf(writer, "%v\n", n-1)
+
+}
