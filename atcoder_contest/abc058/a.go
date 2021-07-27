98ac6b2bad16e2807242fda0f08caa8dd226a75b
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-28 03:01:09 +0900

    [AC] abc058 a

---
+++ atcoder_contest/abc058/a.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+
+	if b-a == c-b {
+		fmt.Fprintf(writer, "%v\n", "YES")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "NO")
+
+	}
+
+}
