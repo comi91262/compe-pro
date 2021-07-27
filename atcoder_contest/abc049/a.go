20aebe4d55e6c4f4ba393c04a668d2fa334e39ab
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-28 01:46:32 +0900

    [AC] abc049 a

--- atcoder_contest/abc049/a.go
+++ atcoder_contest/abc049/a.go
@@ -32,4 +32,35 @@ func main() {
 		}
 	}
 
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var c string
+	fmt.Fscan(reader, &c)
+
+	switch c {
+	case "a":
+		fmt.Fprintf(writer, "%v\n", "vowel")
+	case "i":
+		fmt.Fprintf(writer, "%v\n", "vowel")
+	case "u":
+		fmt.Fprintf(writer, "%v\n", "vowel")
+	case "e":
+		fmt.Fprintf(writer, "%v\n", "vowel")
+	case "o":
+		fmt.Fprintf(writer, "%v\n", "vowel")
+	default:
+		fmt.Fprintf(writer, "%v\n", "consonant")
+	}
 }
