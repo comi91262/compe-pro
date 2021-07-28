c2b35937404d6fbcbb1f521a63552d32f3c1d000
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-28 15:53:05 +0900

    [AC] abc136 a

---
+++ atcoder_contest/abc136/a.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+
+func main() {
+	defer writer.Flush()
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+
+	fmt.Fprintf(writer, "%v\n", max(b+c-a, 0))
+}
+
+
