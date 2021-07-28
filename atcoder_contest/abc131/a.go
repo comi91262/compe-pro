95b534ac9d92c2516dec7d4e6a54026ca765d4ad
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-28 16:19:35 +0900

    [AC] abc131 a

---
+++ atcoder_contest/abc131/a.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var s string
+	fmt.Fscan(reader, &s)
+
+	if s[0] == s[1] || s[1] == s[2] || s[2] == s[3] {
+		fmt.Fprintf(writer, "%v\n", "Bad")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "Good")
+	}
+
+}
