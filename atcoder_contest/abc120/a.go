9fb89e73e860335192b25d540fb302ae4f4af97e
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-28 16:43:26 +0900

    [AC] abc120 a

---
+++ atcoder_contest/abc120/a.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+
+func main() {
+	defer writer.Flush()
+
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+
+	fmt.Fprintf(writer, "%v\n", min(c, b/a))
+
+}
