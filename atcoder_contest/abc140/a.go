137250e6060cf26357075922d18b708ab7827fe0
comi19 <hgKaguyaToupako4er@gmail.com>
2021-07-30 04:07:32 +0900

    [AC] abc140 a

---
+++ atcoder_contest/abc140/a.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func pow(a, x int) int {
+	r := 1
+	for x > 0 {
+		if x&1 == 1 {
+			r *= a
+		}
+		a *= a
+		x >>= 1
+	}
+	return r
+}
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+
+	fmt.Fprintf(writer, "%v\n", pow(n, 3))
+}
