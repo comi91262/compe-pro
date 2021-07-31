71c1f5f12783266954d417ba420581c04fe948ff
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-01 08:55:04 +0900

    [AC] abc070 b

---
+++ atcoder_contest/abc070/b.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+func main() {
+	defer writer.Flush()
+
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+	var d int
+	fmt.Fscan(reader, &d)
+
+	mx := max(c, a)
+	mn := min(b, d)
+
+	fmt.Fprintf(writer, "%v\n", max(0, mn-mx))
+
+}
