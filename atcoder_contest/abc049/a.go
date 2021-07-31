be892e7572ad2d1da4749d84185defda820e4ecb
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-08-01 04:19:44 +0900

    Add

--- atcoder_contest/abc049/a.go
+++ atcoder_contest/abc049/a.go
@@ -1,40 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-)
-
-func main() {
-	var s string
-	fmt.Scan(&s)
-
-	t := ""
-	for {
-		if strings.HasSuffix(s, "dreamer"+t) {
-			t = "dreamer" + t
-
-		} else if strings.HasSuffix(s, "dream"+t) {
-			t = "dream" + t
-
-		} else if strings.HasSuffix(s, "eraser"+t) {
-			t = "eraser" + t
-
-		} else if strings.HasSuffix(s, "erase"+t) {
-			t = "erase" + t
-		} else {
-			if s == t {
-				fmt.Println("YES")
-			} else {
-				fmt.Println("NO")
-			}
-			break
-		}
-	}
-
-}package main
-
-import (
 	"bufio"
 	"fmt"
 	"os"
