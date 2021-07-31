4ca27403ce27fec0fea360aa52bced5b579165dc
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-08-01 03:48:04 +0900

    Update

--- algo/bfs.go
+++ algo/bfs.go
@@ -14,7 +14,7 @@ func bfs(start int, g [][]int) {
 }
 
 // 単一コストの最短経路問題を BFS でとく
-func bfsMinDist(start int, g [][]int) {
+func bfsMinDist(start int, g [][]int) []int {
 	const inf = -(1 << 60)
 	q := queue{}
 	q.push(start)
@@ -34,9 +34,12 @@ func bfsMinDist(start int, g [][]int) {
 			}
 		}
 	}
+
+	return dist
 }
 
-func bfs2(h, w int, sx, sy, ex, ey int, s [][]string) {
+// 単一コストの最短経路問題を BFS でとく (二次元版)
+func bfs2MinDist(h, w int, sx, sy, ex, ey int) [][]int {
 	const inf = 1 << 60
 	var dx = [4]int{1, 0, -1, 0}
 	var dy = [4]int{0, 1, 0, -1}
@@ -46,79 +49,48 @@ func bfs2(h, w int, sx, sy, ex, ey int, s [][]string) {
 		dist[i] = make([]int, w)
 		for j := 0; j < w; j++ {
 			dist[i][j] = inf
-			dist[i][j] = inf
-			dist[i][j] = inf
-			dist[i][j] = inf
 		}
 	}
 
-	deq := create(4080000)
+	q := queue2{}
 	dist[sx][sy] = 0
-	deq.pushFront(state{sx, sy})
+	q.push(point{sx, sy})
 
-	for !deq.empty() {
-		u := deq.popFront()
+	for !q.empty() {
+		u := q.pop()
 		for i := 0; i < 4; i++ {
 			tx := u.x + dx[i]
 			ty := u.y + dy[i]
-			cost := dist[u.x][u.y]
-			if 0 <= tx && tx < h && 0 <= ty && ty < w && s[tx][ty] == "." && dist[tx][ty] > cost {
-				dist[tx][ty] = cost
-				// if u != i {
-				// 	deq.pushBack(state{tx, ty, i})
-				// } else {
-				// 	deq.pushFront(state{tx, ty, i})
-				// }
+			if 0 <= tx && tx < h && 0 <= ty && ty < w && dist[tx][ty] == inf && dist[u.x][u.y]+1 <= dist[tx][ty] {
+				dist[tx][ty] = dist[u.x][u.y] + 1
+				q.push(point{tx, ty})
 			}
 		}
 	}
+
+	return dist
 }
 
-type state struct {
-	x int
-	y int
-}
+// 経路復元
+func restore(h, w int, x, y int, dist [][]int) []point {
+	r := []point{{x, y}}
+	var dx = [4]int{1, 0, -1, 0}
+	var dy = [4]int{0, 1, 0, -1}
 
-type Deque struct {
-	cl   int
-	cr   int
-	data []state
-}
+	c := dist[x][y]
 
-func (d *Deque) pushFront(x state) {
-	d.cl--
-	d.data[d.cl] = x
-}
+	for c > 0 {
+		for i := 0; i < 4; i++ {
+			tx := x + dx[i]
+			ty := y + dy[i]
 
-func (d *Deque) pushBack(x state) {
-	d.data[d.cr] = x
-	d.cr++
-}
+			if 0 <= tx && tx < h && 0 <= ty && ty < w && dist[tx][ty] == c-1 {
+				r = append(r, point{tx, ty})
+				c--
+				x, y = tx, ty
+			}
+		}
+	}
 
-func (d *Deque) popFront() state {
-	v := d.data[d.cl]
-	d.cl++
-	return v
-}
-
-func (d *Deque) popBack() state {
-	v := d.data[d.cr-1]
-	d.cr--
-	return v
-}
-
-func (d *Deque) size() int {
-	return d.cr - d.cl
-}
-
-func (d *Deque) empty() bool {
-	return d.size() == 0
-}
-
-func (d *Deque) get(x int) state {
-	return d.data[d.cl+x-1]
-}
-
-func create(size int) Deque {
-	return Deque{cl: size, cr: size, data: make([]state, size*2, size*2)}
+	return r
 }
--- algo/queue.go
+++ algo/queue.go
@@ -15,3 +15,24 @@ func (q *queue) pop() int {
 func (q *queue) empty() bool {
 	return len(*q) == 0
 }
+
+type point struct {
+	x int
+	y int
+}
+
+type queue2 []point
+
+func (q *queue2) push(n point) {
+	*q = append(*q, n)
+}
+
+func (q *queue2) pop() point {
+	v := (*q)[0]
+	(*q) = (*q)[1:]
+	return v
+}
+
+func (q *queue2) empty() bool {
+	return len(*q) == 0
+}
