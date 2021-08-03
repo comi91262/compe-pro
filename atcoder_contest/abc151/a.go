76fef72c32fba14be85e44d03bc9577426a25e21
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-03 22:25:23 +0900

    [AC] abc151 a

---
+++ atcoder_contest/abc151/a.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var s string
+	fmt.Fscan(reader, &s)
+
+	fmt.Fprintf(writer, "%v\n", string(s[0]+1))
+
+}
