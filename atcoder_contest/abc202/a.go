0aca2e955fb3b009ef878c3b3871c11ddc50a177
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-10 03:08:56 +0900

    [AC] abc202 a

---
+++ atcoder_contest/abc202/a.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+	fmt.Fprintf(writer, "%v\n", 21-a-b-c)
+}
