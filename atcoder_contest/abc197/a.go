19e0ceb58d84dade2f5bf775f3e91e9a78e4eb8f
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-10 03:23:34 +0900

    [AC] abc197 a

---
+++ atcoder_contest/abc197/a.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var ss string
+	fmt.Fscan(reader, &ss)
+	var s = strings.Split(ss, "")
+
+	fmt.Fprintf(writer, "%v\n", s[1]+s[2]+s[0])
+}
