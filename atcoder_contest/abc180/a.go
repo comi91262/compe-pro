f5ae774506200549ae3846326482bc031bdf828c
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-09 18:40:38 +0900

    [AC] abc180 a

---
+++ atcoder_contest/abc180/a.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+
+	fmt.Fprintf(writer, "%v\n", n-a+b)
+}
