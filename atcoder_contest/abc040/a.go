ab4f7bbcbec1e0468143d82b1af260b40f7a38e7
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-10 03:05:23 +0900

    [AC] abc040 a

---
+++ atcoder_contest/abc040/a.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var x int
+	fmt.Fscan(reader, &x)
+
+	fmt.Fprintf(writer, "%v\n", min(x-1, n-x))
+}
