0f66137deb8276838b5246bafed14d017f08fd89
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-09 18:41:51 +0900

    [AC] abc181 a

---
+++ atcoder_contest/abc181/a.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	if n%2 == 0 {
+		fmt.Fprintf(writer, "%v\n", "White")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "Black")
+	}
+}
