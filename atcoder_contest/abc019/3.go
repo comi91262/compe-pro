24fbc8288d7952ce400e57011e1cb15ae82a51c6
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-11 11:05:01 +0900

    [AC] abc019 3

---
+++ atcoder_contest/abc019/3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+	for i := 0; i < n; i++ {
+		for a[i]%2 == 0 {
+			a[i] /= 2
+		}
+	}
+
+	m := map[int]int{}
+	for i := 0; i < n; i++ {
+		m[a[i]]++
+	}
+
+	fmt.Fprintf(writer, "%v\n", len(m))
+}
