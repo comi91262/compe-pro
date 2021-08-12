c834e46f845f8066dd9eb303b1ba1ba2a40fa809
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-12 22:12:55 +0900

    [AC] abc102 b

---
+++ atcoder_contest/abc102/b.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func abs(x int) int {
+	if x >= 0 {
+		return x
+	} else {
+		return x * -1
+	}
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	ans := 0
+	for i := range a {
+		for j := range a {
+			if i == j {
+				continue
+			}
+			ans = max(ans, abs(a[i]-a[j]))
+		}
+	}
+	fmt.Fprintf(writer, "%v\n", ans)
+}
