c2a551500ef5dfc0724ff072f2967c0bf5e6c3eb
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-20 01:30:40 +0900

    [AC] m_solutions2020 a

---
+++ atcoder_contest/m_solutions2020/a.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var x int
+	fmt.Fscan(reader, &x)
+
+	fmt.Fprintf(writer, "%v\n", 8-(x-400)/200)
+
+}
