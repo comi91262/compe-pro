6bf7dd86cc5dec4142ab8fa3b034b7c85e6ee41d
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-19 23:15:17 +0900

    [AC] caddi2018b a

---
+++ atcoder_contest/caddi2018b/a.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+
+	ans := 0
+	for n > 0 {
+		if n%10 == 2 {
+			ans++
+		}
+		n /= 10
+	}
+
+	fmt.Fprintf(writer, "%v\n", ans)
+}
