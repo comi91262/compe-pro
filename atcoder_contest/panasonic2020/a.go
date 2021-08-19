e945f147d1b38ccfa300ee86c5e828be5a5316c2
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-19 12:18:50 +0900

    [AC] panasonic2020 a

---
+++ atcoder_contest/panasonic2020/a.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var k int
+	fmt.Fscan(reader, &k)
+	x := []int{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51}
+	fmt.Fprintf(writer, "%v\n", x[k-1])
+
+}
