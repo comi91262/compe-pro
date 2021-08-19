223febdef77dae6b3718d831ffdd515ad12cec08
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-20 01:33:30 +0900

    [AC] mujin_pc_2018 a

---
+++ atcoder_contest/mujin_pc_2018/a.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var s string
+	fmt.Fscan(reader, &s)
+
+	if strings.HasPrefix(s, "MUJIN") {
+		fmt.Fprintf(writer, "%v\n", "Yes")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "No")
+	}
+}
