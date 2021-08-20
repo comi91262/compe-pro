71f6b4567eb4c1a21025f7249a200a51ea718045
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-20 12:57:41 +0900

    [AC] diverta2019_2 a

---
+++ atcoder_contest/diverta2019_2/a.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var k int
+	fmt.Fscan(reader, &k)
+
+	if k == 1 {
+		fmt.Fprintf(writer, "%v\n", 0)
+	} else {
+		fmt.Fprintf(writer, "%v\n", n-k)
+	}
+}
