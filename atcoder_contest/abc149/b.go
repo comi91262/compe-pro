6e4bc304c87ec37ccd25d26d87f79aa88d475420
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-24 00:06:57 +0900

    [AC] abc149 b

---
+++ atcoder_contest/abc149/b.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+
+func main() {
+	defer writer.Flush()
+
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var k int
+	fmt.Fscan(reader, &k)
+
+	fmt.Fprintf(writer, "%v ", a-min(a, k))
+	k -= min(a, k)
+
+	fmt.Fprintf(writer, "%v", b-min(b, k))
+
+}
