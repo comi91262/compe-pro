c614e9c79631577dfe8bf857b848b9a60e9798e8
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-24 03:43:38 +0900

    [AC] abc156 b

---
+++ atcoder_contest/abc156/b.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var k int
+	fmt.Fscan(reader, &k)
+
+	fmt.Fprintf(writer, "%v\n", len(toDigits(n, k)))
+}
+
+func toDigits(x, base int) []int {
+	if x == 0 {
+		return []int{0}
+	}
+
+	r := make([]int, 0)
+	for x != 0 {
+		r = append(r, x%base)
+		x = x / base
+	}
+
+	return r
+}
