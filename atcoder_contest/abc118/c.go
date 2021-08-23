dc8f6a468d8ef39c10bc071c5f3ad28787a691fa
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-23 13:22:40 +0900

    [AC] abc118 c

---
+++ atcoder_contest/abc118/c.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	ans := a[0]
+	for i := 0; i < n; i++ {
+		ans = gcd(ans, a[i])
+	}
+
+	fmt.Fprintf(writer, "%v\n", ans)
+}
