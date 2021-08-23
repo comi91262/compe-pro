6f3f05f07604168090b1460b91803b9a3b0c0346
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-24 05:45:36 +0900

    [AC] abc163 b

---
+++ atcoder_contest/abc163/b.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func sum(a []int) int {
+	r := 0
+	for i := range a {
+		r += a[i]
+	}
+	return r
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var m int
+	fmt.Fscan(reader, &m)
+
+	var a = make([]int, m)
+	for i := 0; i < m; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	tot := sum(a)
+
+	fmt.Fprintf(writer, "%v\n", max(-1, n-tot))
+
+}
