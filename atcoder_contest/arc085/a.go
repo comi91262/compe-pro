c56da3543a618ce9efd30c251a4f53c012d2af35
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-23 15:53:11 +0900

    [AC] arc085 a

---
+++ atcoder_contest/arc085/a.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func pow(a, x int) int {
+	r := 1
+	for x > 0 {
+		if x&1 == 1 {
+			r *= a
+		}
+		a *= a
+		x >>= 1
+	}
+	return r
+}
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var m int
+	fmt.Fscan(reader, &m)
+
+	fmt.Fprintf(writer, "%v\n", pow(2, m)*(m*1900+(n-m)*100))
+
+}
