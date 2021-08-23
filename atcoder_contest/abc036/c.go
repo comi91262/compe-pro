94005c0a060e27bc9274795d5ce3443e71bacd9d
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-24 08:03:07 +0900

    [AC] abc036 c

---
+++ atcoder_contest/abc036/c.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func abs(x int) int {
+	if x >= 0 {
+		return x
+	} else {
+		return x * -1
+	}
+}
+
+func lowerBound(value, boader int) bool {
+	return boader <= value
+}
+
+func binarySearch(a []int, boader int, criteria func(value, boader int) bool) int {
+	ng := -1
+	ok := len(a)
+
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if criteria(a[mid], boader) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+	return ok
+}
+func unique(a []int) []int {
+	r := make([]int, 0)
+	m := make(map[int]bool, 0)
+
+	for _, e := range a {
+		if !m[e] {
+			m[e] = true
+			r = append(r, e)
+		}
+	}
+
+	return r
+}
+
+func compression(a []int) []int {
+	var n = len(a)
+	var p = make([]int, n)
+
+	for i := 0; i < n; i++ {
+		p[i] = a[i]
+	}
+
+	sort.Ints(p)
+	p = unique(p)
+
+	r := make([]int, n)
+	for i := 0; i < n; i++ {
+		r[i] = binarySearch(p, a[i], lowerBound)
+	}
+
+	return r
+}
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	b := compression(a)
+	for i := 0; i < n; i++ {
+		fmt.Fprintf(writer, "%v\n", b[i])
+	}
+}
