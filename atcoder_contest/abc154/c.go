e732b07f040d931cd9096f1f01e97254987a415d
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-25 04:21:34 +0900

    [AC] abc154 c

---
+++ atcoder_contest/abc154/c.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+	m := map[int]int{}
+	for i := 0; i < n; i++ {
+		m[a[i]]++
+	}
+
+	if len(m) == n {
+		fmt.Fprintf(writer, "%v\n", "YES")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "NO")
+	}
+}
