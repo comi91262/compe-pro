273ad3216d05743441d5d865398323d2dcd62062
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-25 13:02:03 +0900

    [AC] abc178 b

---
+++ atcoder_contest/abc178/b.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+func main() {
+	defer writer.Flush()
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+	var d int
+	fmt.Fscan(reader, &d)
+
+	fmt.Fprintf(writer, "%v\n", max(a*d, b*d, a*c, b*c))
+}
