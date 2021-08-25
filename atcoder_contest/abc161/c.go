94cb1be439dfbe1c5b8deddc18a8b936a34e9f0e
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-25 16:00:55 +0900

    [AC] abc161 c

---
+++ atcoder_contest/abc161/c.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func abs(x int) int {
+	if x >= 0 {
+		return x
+	} else {
+		return x * -1
+	}
+}
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var k int
+	fmt.Fscan(reader, &k)
+
+	d := n % k
+
+	fmt.Fprintf(writer, "%v\n", min(d, abs(d-k)))
+
+}
