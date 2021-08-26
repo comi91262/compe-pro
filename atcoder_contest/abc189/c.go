c11a3a5393bf451644752172dc8a5f89eab3d959
comi19 <hgKaguyaToupako4er@gmail.com>
2021-08-26 14:49:11 +0900

    [AC] abc189 c

---
+++ atcoder_contest/abc189/c.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	ans := 0
+	for l := 0; l < n; l++ {
+		mn := a[l]
+		for r := l; r < n; r++ {
+			mn = min(mn, a[r])
+			ans = max(ans, mn*(r-l+1))
+		}
+	}
+
+	fmt.Fprintf(writer, "%v\n", ans)
+}
+
