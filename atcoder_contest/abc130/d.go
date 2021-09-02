2ed4795e4ad497ed78ce3bba11402249a72cd68d
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-02 10:19:33 +0900

    [AC] abc130 d

---
+++ atcoder_contest/abc130/d.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+type queue []int
+
+func (q *queue) push(n int) {
+	*q = append(*q, n)
+}
+
+func (q *queue) pop() int {
+	v := (*q)[0]
+	(*q) = (*q)[1:]
+	return v
+}
+
+func (q *queue) empty() bool {
+	return len(*q) == 0
+}
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var k int
+	fmt.Fscan(reader, &k)
+
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	ans := 0
+	sum := 0
+	q := queue{}
+	for i := 0; i < n; i++ {
+		q.push(a[i])
+		sum += a[i]
+		for !q.empty() && sum >= k {
+			ans += n - (i + 1) + 1
+			sum -= q.pop()
+		}
+	}
+
+	fmt.Fprintf(writer, "%v\n", ans)
+}
