99ded06f5f205a99443ce75bd76ad26d68858b18
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-09 00:19:03 +0900

    [AC] practice 1

---
+++ atcoder_contest/practice/1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+	var s string
+	fmt.Fscan(reader, &s)
+	fmt.Fprintf(writer, "%v %v\n", a+b+c, s)
+}
