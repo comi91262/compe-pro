3afb91a123d5a10ad44459a47d53b0926798d7e7
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-15 05:28:52 +0900

    [AC] chokudai_S001 e

---
+++ atcoder_contest/chokudai_S001/e.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	for i := 0; i < n; i++ {
+		if a[i] == 1 {
+			fmt.Fprintf(writer, "%v\n", i+1)
+			return
+		}
+
+	}
+}
