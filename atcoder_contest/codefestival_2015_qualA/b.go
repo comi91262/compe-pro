0dfdf8b3f228e44c78edee0827888d80196b013f
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-15 04:06:37 +0900

    [AC] codefestival_2015_qualA b

---
+++ atcoder_contest/codefestival_2015_qualA/b.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func pow(a, x int) int {
+	r := 1
+	for x > 0 {
+		if x&1 == 1 {
+			r *= a
+		}
+		a *= a
+		x >>= 1
+	}
+	return r
+}
+
+func main() {
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	var a = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans += a[i] * pow(2, n-i-1)
+	}
+	fmt.Fprintf(writer, "%v\n", ans)
+}
