04992d549245564125cd9d4a3d57bccaba35c300
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-19 16:46:44 +0900

    [AC] past202107 b

---
+++ atcoder_contest/past202107/b.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	var c int
+	fmt.Fscan(reader, &c)
+
+	for a > b*c {
+		a--
+	}
+	fmt.Fprintf(writer, "%v\n", float64(a)/float64(b))
+}
