42c0d30b1b212114cb253e7e5405378fd0c4b26f
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-21 16:59:49 +0900

    [AC] arc002 1

--- atcoder_contest/arc002/1.go
+++ atcoder_contest/arc002/1.go
@@ -42,3 +42,33 @@ func main() {
 	}
 
 }
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func isLeapYear(y int) (isOk bool) {
+	isOk = y%4 == 0
+	isOk = isOk && y%100 != 0
+	isOk = isOk || y%400 == 0
+	return
+}
+
+func main() {
+	defer writer.Flush()
+	var y int
+	fmt.Fscan(reader, &y)
+
+	if isLeapYear(y) {
+		fmt.Fprintf(writer, "%v\n", "YES")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "NO")
+	}
+
+}
