dce963c752b8eb481589dcbb42d08e777651dec5
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-22 21:09:29 +0900

    [AC] arc030 1

---
+++ atcoder_contest/arc030/1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscan(reader, &n)
+	var k int
+	fmt.Fscan(reader, &k)
+
+	if n/2 >= k {
+		fmt.Fprintf(writer, "%v\n", "YES")
+	} else {
+		fmt.Fprintf(writer, "%v\n", "NO")
+	}
+}
