e81db8352c50a092e82f96a7a8c883201c98cd6b
comi19 <hgKaguyaToupako4er@gmail.com>
2021-09-26 02:20:00 +0900

    [AC] indeednow_2015_qualb 2

---
+++ atcoder_contest/indeednow_2015_qualb/2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var s string
+	fmt.Fscan(reader, &s)
+	var t string
+	fmt.Fscan(reader, &t)
+
+	for i := 0; i < len(s); i++ {
+		if s == t {
+			fmt.Fprintf(writer, "%v\n", i)
+			return
+		}
+		s = string(s[len(s)-1]) + s[:len(s)-1]
+	}
+
+	fmt.Fprintf(writer, "%v\n", -1)
+}
