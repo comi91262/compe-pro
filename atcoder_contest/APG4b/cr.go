28972ccafccc932d0aadcd45a019e3317030a37e
comi19 <hgKaguyaToupako4er@gmail.com>
2021-10-01 20:01:01 +0900

    [AC] APG4b cr

---
+++ atcoder_contest/APG4b/cr.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var a int
+	fmt.Fscan(reader, &a)
+	var b int
+	fmt.Fscan(reader, &b)
+	fmt.Fprintf(writer, "%v\n", a+b)
+
+}
