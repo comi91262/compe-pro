9f5422000e25490a61d1797d54666148c31a2f8a
comi19 <hgKaguyaToupako4er@gmail.com>
2021-10-09 10:52:43 +0900

    [AC] joi2020_yo1b b

---
+++ atcoder_contest/joi2020_yo1b/b.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func scanInt() int       { sc.Scan(); return parseInt(sc.Bytes()) }
+func scanString() string { sc.Scan(); return string(sc.Bytes()) }
+func scanFloat() float64 { sc.Scan(); return parseFloat(sc.Bytes()) }
+func scanInts(n int) (ints []int) {
+	ints = make([]int, n)
+	for i := 0; i < n; i++ {
+		ints[i] = scanInt()
+	}
+	return
+}
+func scanStrings(n int) (st []string) {
+	st = make([]string, n)
+	for i := 0; i < n; i++ {
+		st[i] = scanString()
+	}
+	return
+}
+func scanFloats(n int) (fs []float64) {
+	fs = make([]float64, n)
+	for i := 0; i < n; i++ {
+		fs[i] = scanFloat()
+	}
+	return fs
+}
+
+func parseInt(b []byte) (n int) {
+	for _, ch := range b {
+		ch -= '0'
+		if ch <= 9 {
+			n = n*10 + int(ch)
+		}
+	}
+	if b[0] == '-' {
+		return -n
+	}
+	return
+}
+
+var float64pow10 = []float64{
+	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6,
+	1e7, 1e8, 1e9, 1e10, 1e11, 1e12,
+	1e13, 1e14, 1e15, 1e16, 1e17, 1e18,
+	1e19, 1e20, 1e21, 1e22,
+}
+
+func parseFloat(b []byte) float64 {
+	f, dot := 0.0, 0
+	for i, ch := range b {
+		if ch == '.' {
+			dot = i + 1
+			continue
+		}
+		if ch -= '0'; ch <= 9 {
+			f = f*10 + float64(ch)
+		}
+	}
+	if dot != 0 {
+		f /= float64pow10[len(b)-dot]
+	}
+	if b[0] == '-' {
+		return -f
+	}
+	return f
+}
+func swap(a []string, i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func reverse(a []string, i, j int) {
+	for k := 0; k < (j-i+1)/2; k++ {
+		swap(a, i+k, j-k)
+	}
+}
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	scanInt()
+	a := scanInt()
+	b := scanInt()
+	s := strings.Split(scanString(), "")
+	reverse(s, a-1, b-1)
+	fmt.Fprintf(wr, "%v\n", strings.Join(s, ""))
+}
