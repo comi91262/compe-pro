9820e5ec57694e98548c206490c758c1619aeafa
comi19 <hgKaguyaToupako4er@gmail.com>
2021-10-11 11:41:47 +0900

    [AC] abc073 d

---
+++ atcoder_contest/abc073/d.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func scanInt() int       { sc.Scan(); return parseInt(sc.Bytes()) }
+func scanString() string { sc.Scan(); return string(sc.Bytes()) }
+func scanFloat() float64 { sc.Scan(); return parseFloat(sc.Bytes()) }
+func scanInts(n int) (ints []int) {
+	ints = make([]int, n)
+	for i := 0; i < n; i++ {
+		ints[i] = scanInt()
+	}
+	return
+}
+func scanStrings(n int) (st []string) {
+	st = make([]string, n)
+	for i := 0; i < n; i++ {
+		st[i] = scanString()
+	}
+	return
+}
+func scanFloats(n int) (fs []float64) {
+	fs = make([]float64, n)
+	for i := 0; i < n; i++ {
+		fs[i] = scanFloat()
+	}
+	return fs
+}
+
+func parseInt(b []byte) (n int) {
+	for _, ch := range b {
+		ch -= '0'
+		if ch <= 9 {
+			n = n*10 + int(ch)
+		}
+	}
+	if b[0] == '-' {
+		return -n
+	}
+	return
+}
+
+var float64pow10 = []float64{
+	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6,
+	1e7, 1e8, 1e9, 1e10, 1e11, 1e12,
+	1e13, 1e14, 1e15, 1e16, 1e17, 1e18,
+	1e19, 1e20, 1e21, 1e22,
+}
+
+func parseFloat(b []byte) float64 {
+	f, dot := 0.0, 0
+	for i, ch := range b {
+		if ch == '.' {
+			dot = i + 1
+			continue
+		}
+		if ch -= '0'; ch <= 9 {
+			f = f*10 + float64(ch)
+		}
+	}
+	if dot != 0 {
+		f /= float64pow10[len(b)-dot]
+	}
+	if b[0] == '-' {
+		return -f
+	}
+	return f
+}
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+
+func warshallFloyd(d [][]int, n int) [][]int {
+	for k := 0; k < n; k++ {
+		for i := 0; i < n; i++ {
+			for j := 0; j < n; j++ {
+				d[i][j] = min(d[i][j], d[i][k]+d[k][j])
+			}
+		}
+	}
+
+	return d
+}
+func fact(n int) int {
+	r := 1
+	for n > 0 {
+		r *= n
+		n--
+	}
+	return r
+}
+
+func swap(a []int, i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func reverse(a []int, i, j int) {
+	for k := 0; k < (j-i+1)/2; k++ {
+		swap(a, i+k, j-k)
+	}
+}
+func nextPermutation(a []int) {
+	var lastIndex = len(a) - 1
+
+	if lastIndex < 1 {
+		return
+	}
+
+	var i = lastIndex - 1
+	for i >= 0 && a[i] >= a[i+1] {
+		i -= 1
+	}
+	if i < 0 {
+		reverse(a, 0, lastIndex)
+		return
+	}
+
+	var j = lastIndex
+	for j > i+1 && a[j] <= a[i] {
+		j -= 1
+	}
+
+	swap(a, i, j)
+	reverse(a, i+1, lastIndex)
+}
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 10000), 1001001)
+
+	n := scanInt()
+	m := scanInt()
+	l := scanInt()
+	r := scanInts(l)
+	for i := 0; i < l; i++ {
+		r[i]--
+	}
+
+	var dist = make([][]int, n)
+	for i := 0; i < n; i++ {
+		dist[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			dist[i][j] = 1 << 60
+		}
+	}
+	for i := 0; i < m; i++ {
+		a := scanInt()
+		b := scanInt()
+		c := scanInt()
+		a--
+		b--
+		dist[a][b] = c
+		dist[b][a] = c
+	}
+	dist = warshallFloyd(dist, n)
+
+	ans := 1 << 60
+
+	for i := 0; i < fact(l); i++ {
+		d := 0
+		a := r[0]
+		for i := 1; i < l; i++ {
+			d += dist[a][r[i]]
+			a = r[i]
+		}
+		ans = min(ans, d)
+		nextPermutation(r)
+	}
+	fmt.Fprintf(wr, "%v\n", ans)
+}
+
