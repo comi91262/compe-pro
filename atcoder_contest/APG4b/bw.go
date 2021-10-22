4e85995832763c1a1c35fc997ba770c3b6d30ef6
comi19 <hgKaguyaToupako4er@gmail.com>
2021-10-23 06:05:05 +0900

    [AC] APG4b bw

---
+++ atcoder_contest/APG4b/bw.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func scanInt() int       { sc.Scan(); return parseInt(sc.Bytes()) }
+func scanString() string { sc.Scan(); return string(sc.Bytes()) }
+func scanFloat() float64 { sc.Scan(); return parseFloat(sc.Bytes()) }
+func scanInts(n int) (ints []int) {
+	ints = make([]int, n)
+	for i := 0; i < n; i++ {
+		ints[i] = scanInt()
+	}
+	return
+}
+
+func scanPairInts(n int) (a, b []int) {
+	a = make([]int, n)
+	b = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+		b[i] = scanInt()
+	}
+	return
+}
+
+func scanStrings(n int) (st []string) {
+	st = make([]string, n)
+	for i := 0; i < n; i++ {
+		st[i] = scanString()
+	}
+	return
+}
+func scanFloats(n int) (fs []float64) {
+	fs = make([]float64, n)
+	for i := 0; i < n; i++ {
+		fs[i] = scanFloat()
+	}
+	return fs
+}
+
+func parseInt(b []byte) (n int) {
+	for _, ch := range b {
+		ch -= '0'
+		if ch <= 9 {
+			n = n*10 + int(ch)
+		}
+	}
+	if b[0] == '-' {
+		return -n
+	}
+	return
+}
+
+var float64pow10 = []float64{
+	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6,
+	1e7, 1e8, 1e9, 1e10, 1e11, 1e12,
+	1e13, 1e14, 1e15, 1e16, 1e17, 1e18,
+	1e19, 1e20, 1e21, 1e22,
+}
+
+func parseFloat(b []byte) float64 {
+	f, dot := 0.0, 0
+	for i, ch := range b {
+		if ch == '.' {
+			dot = i + 1
+			continue
+		}
+		if ch -= '0'; ch <= 9 {
+			f = f*10 + float64(ch)
+		}
+	}
+	if dot != 0 {
+		f /= float64pow10[len(b)-dot]
+	}
+	if b[0] == '-' {
+		return -f
+	}
+	return f
+}
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+
+func expInt(env map[string]int) (r int) {
+	r = v(env)
+	for op := scanString(); op != ";"; op = scanString() {
+		switch op {
+		case "+":
+			r += v(env)
+		case "-":
+			r -= v(env)
+		}
+	}
+	return
+}
+
+func expVec(env map[string]int, enV map[string][]int) []int {
+	v := ve(env, enV)
+	v1 := make([]int, len(v))
+	copy(v1, v)
+
+	for op := scanString(); op != ";"; op = scanString() {
+		switch op {
+		case "+":
+			v2 := ve(env, enV)
+			for i := 0; i < len(v); i++ {
+				v1[i] += v2[i]
+			}
+		case "-":
+			v2 := ve(env, enV)
+			for i := 0; i < len(v); i++ {
+				v1[i] -= v2[i]
+			}
+		}
+	}
+	return v1
+}
+
+func ve(env map[string]int, enV map[string][]int) []int {
+	s := scanString()
+	if s != "[" {
+		return enV[s]
+	}
+	r := []int{v(env)}
+	for comma := scanString(); comma != "]"; comma = scanString() {
+		r = append(r, v(env))
+	}
+	return r
+}
+
+func v(env map[string]int) int {
+	s := scanString()
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return env[s]
+	}
+	return i
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 10000), 1001001)
+	n := scanInt()
+	env := map[string]int{}
+	enV := map[string][]int{}
+
+	for i := 0; i < n; i++ {
+		switch scanString() {
+		case "int":
+			x := scanString()
+			scanString()
+			env[x] = expInt(env)
+		case "print_int":
+			fmt.Fprintf(wr, "%v\n", expInt(env))
+		case "vec":
+			a := scanString()
+			scanString()
+			enV[a] = expVec(env, enV)
+		case "print_vec":
+			fmt.Fprintf(wr, "[ ")
+			v := expVec(env, enV)
+			for i := 0; i < len(v); i++ {
+				fmt.Fprintf(wr, "%v ", v[i])
+			}
+			fmt.Fprintf(wr, "]\n")
+		}
+	}
+}
