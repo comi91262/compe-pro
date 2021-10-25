1f8a2c223d332d29e9a55451a13881ef6ad03202
comi19 <hgKaguyaToupako4er@gmail.com>
2021-10-26 03:32:58 +0900

    [AC] dfs a

---
+++ atcoder_contest/dfs/a.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func scanInt() int       { sc.Scan(); return parseInt(sc.Bytes()) }
+func scanString() string { sc.Scan(); return string(sc.Bytes()) }
+func scanFloat() float64 { sc.Scan(); return parseFloat(sc.Bytes()) }
+func scanInts(n int) (ints []int) {
+	ints = make([]int, n)
+	for i := 0; i < n; i++ {
+		ints[i] = scanInt()
+	}
+	return
+}
+
+func scanPairInts(n int) (a, b []int) {
+	a = make([]int, n)
+	b = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+		b[i] = scanInt()
+	}
+	return
+}
+
+func scanStrings(n int) (st []string) {
+	st = make([]string, n)
+	for i := 0; i < n; i++ {
+		st[i] = scanString()
+	}
+	return
+}
+func scanFloats(n int) (fs []float64) {
+	fs = make([]float64, n)
+	for i := 0; i < n; i++ {
+		fs[i] = scanFloat()
+	}
+	return fs
+}
+
+func parseInt(b []byte) (n int) {
+	for _, ch := range b {
+		ch -= '0'
+		if ch <= 9 {
+			n = n*10 + int(ch)
+		}
+	}
+	if b[0] == '-' {
+		return -n
+	}
+	return
+}
+
+var float64pow10 = []float64{
+	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6,
+	1e7, 1e8, 1e9, 1e10, 1e11, 1e12,
+	1e13, 1e14, 1e15, 1e16, 1e17, 1e18,
+	1e19, 1e20, 1e21, 1e22,
+}
+
+func parseFloat(b []byte) float64 {
+	f, dot := 0.0, 0
+	for i, ch := range b {
+		if ch == '.' {
+			dot = i + 1
+			continue
+		}
+		if ch -= '0'; ch <= 9 {
+			f = f*10 + float64(ch)
+		}
+	}
+	if dot != 0 {
+		f /= float64pow10[len(b)-dot]
+	}
+	if b[0] == '-' {
+		return -f
+	}
+	return f
+}
+
+func min(arg ...int) int {
+	min := arg[0]
+	for _, x := range arg {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func max(arg ...int) int {
+	max := arg[0]
+	for _, x := range arg {
+		if max < x {
+			max = x
+		}
+	}
+	return max
+}
+
+// 二次元配列をDFSする
+func dfs2d(h, w, x, y, tx, ty int, s [][]string, used [][]bool) (reached bool) {
+	if used[x][y] {
+		return
+	}
+
+	var dx = [4]int{0, 1, 0, -1}
+	var dy = [4]int{1, 0, -1, 0}
+
+	used[x][y] = true
+	for i := 0; i < 4; i++ {
+		nx := x + dx[i]
+		ny := y + dy[i]
+		if nx < 0 || ny < 0 || nx >= h || ny >= w {
+			continue
+		}
+		if s[nx][ny] == "#" {
+			continue
+		}
+		if nx == tx && ny == ty {
+			reached = true
+		}
+		reached = reached || dfs2d(h, w, nx, ny, tx, ty, s, used)
+	}
+
+	return
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 10000), 1001001)
+	h := scanInt()
+	w := scanInt()
+	s := make([][]string, h)
+	for i := 0; i < h; i++ {
+		s[i] = strings.Split(scanString(), "")
+	}
+
+	sx, sy := 0, 0
+	tx, ty := 0, 0
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if s[i][j] == "s" {
+				s[i][j] = "."
+				sx, sy = i, j
+			}
+			if s[i][j] == "g" {
+				s[i][j] = "."
+				tx, ty = i, j
+			}
+		}
+	}
+	used := make([][]bool, h)
+	for i := 0; i < h; i++ {
+		used[i] = make([]bool, w)
+	}
+	if dfs2d(h, w, sx, sy, tx, ty, s, used) {
+		fmt.Fprintf(wr, "%v\n", "Yes")
+	} else {
+		fmt.Fprintf(wr, "%v\n", "No")
+	}
+
+}
