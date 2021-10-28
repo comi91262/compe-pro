7f7107b34bdc5b61cfd7ea38a69d6bae8e87e4f7
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-10-28 11:49:52 +0900

    Update

--- algo/datastructures/deque.go
+++ algo/datastructures/deque.go
@@ -6,40 +6,52 @@ type Deque struct {
 	data []int
 }
 
-func (d *Deque) pushFront(x int) {
+func (d *Deque) PushFront(x int) {
 	d.cl--
 	d.data[d.cl] = x
 }
 
-func (d *Deque) pushBack(x int) {
+func (d *Deque) PushBack(x int) {
 	d.data[d.cr] = x
 	d.cr++
 }
 
-func (d *Deque) popFront() int {
+func (d *Deque) PopFront() int {
 	v := d.data[d.cl]
 	d.cl++
 	return v
 }
 
-func (d *Deque) popBack() int {
+func (d *Deque) PopBack() int {
 	v := d.data[d.cr-1]
 	d.cr--
 	return v
 }
 
-func (d *Deque) size() int {
+func (d *Deque) Size() int {
 	return d.cr - d.cl
 }
 
-func (d *Deque) empty() bool {
-	return d.size() == 0
+func (d *Deque) Empty() bool {
+	return d.Size() == 0
 }
 
-func (d *Deque) get(x int) int {
+func (d *Deque) Get(x int) int {
 	return d.data[d.cl+x-1]
 }
 
-func create(size int) Deque {
-	return Deque{cl: size, cr: size, data: make([]int, size*2, size*2)}
+func (d *Deque) Debug() (a []int) {
+	a = make([]int, d.Size())
+	for i := d.cl; i < d.cr; i++ {
+		a[i-d.cl] = d.data[i]
+	}
+	return
+}
+
+func NewDeque(n int) *Deque {
+	return &Deque{
+		cl:   n,
+		cr:   n,
+		data: make([]int, n*2, n*2),
+	}
 }
