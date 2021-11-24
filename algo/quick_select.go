bad6fea48f910c402a457c9d9a6f2680e59f114e
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-11-24 23:44:59 +0900

    Update

--- algo/quick_select.go
+++ algo/quick_select.go
@@ -1,12 +1,22 @@
 package algo
 
-// [start, end]
-func insertionSort(a []int, start, end int) {
-	for i := start; i <= end; i++ {
+func insertionSort(a []int) {
+	for i := 0; i < len(a); i++ {
 		j := i
-		for (j > start) && (a[j-1] > a[j]) {
+		for (j > 0) && (a[j-1] > a[j]) {
 			a[j-1], a[j] = a[j], a[j-1]
 			j--
 		}
+	}
+}
+
+func median(a []int) int {
+	n := len(a)
+	insertionSort(a)
+
+	if n%2 == 0 {
+		return a[n/2-1]
+	} else {
+		return a[n/2]
 	}
 }
--- algo/quick_select_test.go
+++ algo/quick_select_test.go
@@ -5,21 +5,29 @@ import "testing"
 func TestInsertionSort(t *testing.T) {
 	a := []int{8, 7, 6, 5, 4, 3, 2, 1}
 
-	insertionSort(a, 0, 1)
-	want := []int{7, 8, 6, 5, 4, 3, 2, 1}
+	insertionSort(a)
+	want := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	if !testEqSlice(a, want) {
 		t.Errorf("Error 0: %v", a)
 	}
+}
 
-	insertionSort(a, 4, 5)
-	want = []int{7, 8, 6, 5, 3, 4, 2, 1}
-	if !testEqSlice(a, want) {
-		t.Errorf("Error 1: %v", a)
+func TestMedian0(t *testing.T) {
+	a := []int{8, 7, 6, 5, 4, 3, 2, 1}
+	got := median(a)
+	want := 4
+
+	if got != want {
+		t.Errorf("Error 0: %v", got)
 	}
+}
 
-	insertionSort(a, 0, len(a)-1)
-	want = []int{1, 2, 3, 4, 5, 6, 7, 8}
-	if !testEqSlice(a, want) {
-		t.Errorf("Error 2: %v", a)
+func TestMedian1(t *testing.T) {
+	a := []int{8, 7, 6, 5, 4, 3, 2}
+	got := median(a)
+	want := 5
+
+	if got != want {
+		t.Errorf("Error 0: %v", got)
 	}
 }
