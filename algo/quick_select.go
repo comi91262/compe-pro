1e4e87f78b00216875b742fc470ced4b660151f6
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-11-24 23:14:38 +0900

    Update insertionSort.go

--- algo/quick_select.go
+++ algo/quick_select.go
@@ -1,9 +1,10 @@
 package algo
 
-func insertionSort(a []int) {
-	for i := 0; i < len(a); i++ {
+// [start, end]
+func insertionSort(a []int, start, end int) {
+	for i := start; i <= end; i++ {
 		j := i
-		for (j > 0) && (a[j-1] > a[j]) {
+		for (j > start) && (a[j-1] > a[j]) {
 			a[j-1], a[j] = a[j], a[j-1]
 			j--
 		}
--- algo/quick_select_test.go
+++ algo/quick_select_test.go
@@ -4,11 +4,22 @@ import "testing"
 
 func TestInsertionSort(t *testing.T) {
 	a := []int{8, 7, 6, 5, 4, 3, 2, 1}
-	want := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
-	insertionSort(a)
-
+	insertionSort(a, 0, 1)
+	want := []int{7, 8, 6, 5, 4, 3, 2, 1}
 	if !testEqSlice(a, want) {
 		t.Errorf("Error 0: %v", a)
+	}
+
+	insertionSort(a, 4, 5)
+	want = []int{7, 8, 6, 5, 3, 4, 2, 1}
+	if !testEqSlice(a, want) {
+		t.Errorf("Error 1: %v", a)
+	}
+
+	insertionSort(a, 0, len(a)-1)
+	want = []int{1, 2, 3, 4, 5, 6, 7, 8}
+	if !testEqSlice(a, want) {
+		t.Errorf("Error 2: %v", a)
 	}
 }
